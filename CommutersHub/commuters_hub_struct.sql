90f88b347e2e1d44feab4525e7fffe5bb1c0e633
Christopher Wong <marvelownz@Christophers-MacBook-Pro-2.local>
2016-01-16 13:58:37 -0500

    database

---
+++ CommutersHub/commuters_hub_struct.sql
@@ -0,0 +1,55 @@
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(225) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `pw_hash` char(225) NOT NULL,
+  `salt` char(225) NOT NULL,
+  `admin` boolean,
+  PRIMARY KEY (`user_id`),
+  UNIQUE (`username`),
+  UNIQUE (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+--
+-- Table structure for table `trip_users`
+--
+
+DROP TABLE IF EXISTS `trip`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trip_users` (
+  `trip_id` int NOT NULL AUTO_INCREMENT,
+  `unix_time` BIGINT UNSIGNED NOT NULL,
+  PRIMARY KEY (`trip_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+--
+-- Table structure for table `trip_users`
+--
+
+DROP TABLE IF EXISTS `trip_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trip_users` (
+  `trip_id` int NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`trip_id, user_id`),
+  CONSTRAINT `trip_users_trip_id_fk` FOREIGN KEY (`trip_id`) REFERENCES `trip` (`trip_id`),
+  CONSTRAINT `trip_users_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
